f5ae56aa77228e06cd24b507ecabd3a84b3c0dd8
Andrey Malakhov <a.malakhov@mobidev.biz>
2016-12-16 13:59:31 +0200

    Update robolectric

--- app/src/test/java/blak/mvx/BaseTest.java
+++ app/src/test/java/blak/mvx/BaseTest.java
@@ -3,12 +3,12 @@ package blak.mvx;
 import blak.mvx.di.TestComponent;
 import org.junit.Before;
 import org.junit.runner.RunWith;
-import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
 import android.support.annotation.CallSuper;
 
-@RunWith(RobolectricGradleTestRunner.class)
+@RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class,
         application = TestApp.class,
         sdk = 21)
